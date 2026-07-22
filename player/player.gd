5ee765aac3f81940c9981c697cc56a7483105a1a
Penguinja8 <144949726+Penguinja8@users.noreply.github.com>
2026-07-22 14:46:50 -0400

    basic player

---
+++ player/player.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+const GRAVITY = 350
+
+const MAX_JUMP = -200
+const MAX_JUMP_CHARGE = 0.5
+var jump_charge = 0.0
+
+const MAX_SPEED = 150
+
+func _physics_process(delta: float):
+	if is_on_floor():
+		velocity.x = 0
+		if Input.is_action_pressed("jump"):
+			jump_charge = move_toward(jump_charge, MAX_JUMP_CHARGE, delta)
+		if Input.is_action_just_released("jump"):
+			velocity.y = MAX_JUMP * (jump_charge/MAX_JUMP_CHARGE)
+			jump_charge = 0.0
+	else:
+		var dir = Input.get_axis("left", "right")
+		velocity.x = MAX_SPEED * dir
+		velocity.y += GRAVITY * delta # add curve later
+	move_and_slide()
