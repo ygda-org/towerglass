860629f2a62d8ad4e50e0fecbe70ad96f2fea3b4
Penguinja8 <144949726+Penguinja8@users.noreply.github.com>
2026-07-22 19:56:10 -0400

    fegwegwe

--- levels/level_objects/turret/turret.gd
+++ levels/level_objects/turret/turret.gd
@@ -15,15 +15,16 @@ func _ready():
 	shoot()
 
 func shoot():
-	print(shot_directions[current_direction_index])
 	$BarrelPivot.rotation = wrapf($BarrelPivot.rotation, -180, 180)
 	if harmonic and current_direction_index == 0 or current_direction_index == len(shot_directions)-1:
 		iter_dir *= -1
 	var tween = get_tree().create_tween()
 	var next_direction_index = (current_direction_index + iter_dir) % len(shot_directions)
 	var next_angle = shot_directions[next_direction_index].angle()
-	if abs(signf(next_angle)*PI*2-next_angle - $BarrelPivot.rotation) < abs(next_angle - $BarrelPivot.rotation):
-		next_angle = signf(next_angle)*PI*2-next_angle
+	var calc_other = signf(next_angle)*PI*2-next_angle
+	print(rad_to_deg(next_angle), " ", rad_to_deg(calc_other), " ", rad_to_deg($BarrelPivot.rotation))
+	if abs(calc_other - $BarrelPivot.rotation) < abs(next_angle - $BarrelPivot.rotation):
+		next_angle = calc_other
 	tween.tween_property($BarrelPivot, "rotation", next_angle, shot_time)
 	tween.tween_callback(shoot.call_deferred)
 	current_direction_index = next_direction_index
