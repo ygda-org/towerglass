7620e69aa4b5db9c30abeddf41b2ec4fcef7f743
CookieFinder23 <190116541+CookieFinder23@users.noreply.github.com>
2026-07-23 11:33:23 -0400

    movable on/off

--- levels/level_objects/movable_platform.gd
+++ levels/level_objects/movable_platform.gd
@@ -1,23 +1,24 @@
 extends Node2D
 
 @export var size: int = 5
+@export var tile: PackedScene
 var start_pos: Vector2
-const MOVABLE_TILE = preload("uid://cp7kql7qpqvxm")
 
 func _ready() -> void:
 	var posx = 0
 	start_pos = position
 	for i in range(size):
-		var tile = MOVABLE_TILE.instantiate()
-		if i == 0:
-			tile.get_node("AnimatedSprite2D").set_animation('left')
-		elif i == size - 1:
-			tile.get_node("AnimatedSprite2D").set_animation('right')
-		else:
-			tile.get_node("AnimatedSprite2D").set_animation('middle')
-		tile.position = Vector2(posx, 0)
+		var addend = tile.instantiate()
+		if addend.name == 'MovableTile':
+			if i == 0:
+				addend.get_node("AnimatedSprite2D").set_animation('left')
+			elif i == size - 1:
+				addend.get_node("AnimatedSprite2D").set_animation('right')
+			else:
+				addend.get_node("AnimatedSprite2D").set_animation('middle')
+		addend.position = Vector2(posx, 0)
 		posx += 16
-		add_child(tile)
+		add_child(addend)
 
 func _physics_process(delta: float) -> void:
 	var i: int = 0
