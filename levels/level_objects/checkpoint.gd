146db776c60c83366aec020fb95df7bfd6cce17f
Kelly <KZ.kellyzhang123@gmail.com>
2026-07-22 20:08:40 -0400

    checkpoint fully works now yayyy this did not need to be 3 separate pushes but hey i never said i was smart did i

--- levels/level_objects/checkpoint.gd
+++ levels/level_objects/checkpoint.gd
@@ -11,6 +11,7 @@ func activate_checkpoint():
 	activated = true
 	$AnimationPlayer.play("activation")
 	GameState.last_location = $RespawnPoint.global_position
+	GameState.player.sand_in_bottom = 0
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
