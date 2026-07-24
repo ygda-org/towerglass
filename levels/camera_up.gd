7e352b49d898958c864462e4f0a3fa4661fccb8f
CookieFinder23 <190116541+CookieFinder23@users.noreply.github.com>
2026-07-24 00:34:47 -0400

    level

--- levels/camera_up.gd
+++ levels/camera_up.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 func _physics_process(delta: float) -> void:
 	if has_overlapping_bodies():
-		GameState.player.get_node("Camera2D").offset.y -= delta * 100
+		GameState.player.get_node("Camera2D").offset.y -= delta * 142
 	else:
-		GameState.player.get_node("Camera2D").offset.y += delta * 100
-	GameState.player.get_node("Camera2D").offset.y = clamp(GameState.player.get_node("Camera2D").offset.y, -128, 0)
+		GameState.player.get_node("Camera2D").offset.y += delta * 142
+	GameState.player.get_node("Camera2D").offset.y = clamp(GameState.player.get_node("Camera2D").offset.y, -142, 0)
