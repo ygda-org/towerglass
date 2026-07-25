8540472acaa276c0a5c6362354b6560fb11ea8ad
MrPuffyWuff <jasper13.com@gmail.com>
2026-07-25 13:15:26 -0400

    exit animation DONE

--- levels/level_objects/level_exit.gd
+++ levels/level_objects/level_exit.gd
@@ -19,18 +19,25 @@ func _on_body_entered(body: Node2D) -> void:
 
 
 func super_cool_exit_animation():
+	var initial_length : float = 4.75
+	
 	GameState.player.sand.play("default")
 	GameState.player.find_child("Anim").play("default")
 	$InwardStar.emitting = true
+	SFX.play(SFX.Labels.WINDUPSPARKLE)
 	var tween : Tween = get_tree().create_tween()
-	tween.tween_property(GameState.player, "global_position", global_position + Vector2(0,2), 5)
+	tween.tween_property(GameState.player, "global_position", global_position + Vector2(0,2), initial_length)
+	var tween2 : Tween = get_tree().create_tween()
+	tween2.tween_property($Clock, "self_modulate", Color(1.0,1.0,1.0,1.0), initial_length)
 	GameState.player.find_child("Camera2D").position = Vector2.ZERO
 	GameState.player.find_child("Camera2D").align()
+	$Clock.speed_scale = 1/initial_length
+	$Clock.play("clock_spin")
 	#var tween2 : Tween = get_tree().create_tween()
 	#tween.tween_property(GameState.player.find_child("Camera2D"), "global_position", global_position + Vector2(0,3), 5)
 	await tween.finished
-	$InwardStar.restart()
-	$InwardStar.emitting = false
+	$Clock.visible = false
+	$InwardStar.visible = false
 	GameState.player.visible = false
 	$Explosion.emitting = true
 	await get_tree().create_timer(1).timeout
