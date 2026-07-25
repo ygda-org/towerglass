c89563c55874846ab318994fe130f0aab490c816
MrPuffyWuff <jasper13.com@gmail.com>
2026-07-25 16:30:17 -0400

    Game refreshes on death now

--- ui/select_button.gd
+++ ui/select_button.gd
@@ -13,6 +13,7 @@ func _ready() -> void:
 
 func _on_pressed() -> void:
 	GameState.current_level = number
+	GameState.current_lvl_path = target
 	SFX.play(SFX.Labels.BUTTONCLICK)
 	Music.start(level_song)
 	SceneSwitcher.go_to_scene(target)
