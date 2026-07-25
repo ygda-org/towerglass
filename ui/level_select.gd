02343c5927fd209ac24bddea7ea620c48ff1bf9b
Penguinja8 <144949726+Penguinja8@users.noreply.github.com>
2026-07-25 18:18:18 -0400

    your skip button

--- ui/level_select.gd
+++ ui/level_select.gd
@@ -2,3 +2,13 @@ extends Control
 
 func _ready():
 	Music.start(Music.Labels.SELECT)
+
+
+func _on_skip_pressed():
+	SFX.play(SFX.Labels.BUTTONCLICK)
+	GameState.max_level_beaten += 1
+	get_tree().change_scene_to_file("uid://bn6f701wdg7wr")
+
+
+func _on_skip_mouse_entered():
+	SFX.play(SFX.Labels.BUTTONHOVER)
