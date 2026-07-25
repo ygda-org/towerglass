2c743ce7675723242fc4fed52c26cce1e41b2ec4
MrPuffyWuff <jasper13.com@gmail.com>
2026-07-25 18:43:51 -0400

    bacgkround temp thinguasidoj

--- levels/level_objects/background.gd
+++ levels/level_objects/background.gd
@@ -2,10 +2,15 @@ extends Node2D
 
 @onready var camera : Camera2D
 
+@onready var b1 = $Offset/Parallax2D/Background
+@onready var b2 = $Offset/Parallax2D2/Gears
+@onready var b3 = $Offset/Parallax2D4/BigPipes
+@onready var b4 = $Offset/Parallax2D3/SmallPipes
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	self.visible = true
-
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
