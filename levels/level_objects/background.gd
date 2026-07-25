f46e5372ecbfc2b142bf6d924dbdf3283d251e32
MrPuffyWuff <jasper13.com@gmail.com>
2026-07-25 00:04:55 -0400

    background parallax!

---
+++ levels/level_objects/background.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+@onready var camera : Camera2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	#if camera == null and GameState.player != null:
+		#camera = GameState.player.find_child("Camera2D")
+	#global_position.y = camera.global_position.y
+	pass
