ddf88316c3de3de19fefc075dfb20a4ed420ea2b
Penguinja8 <144949726+Penguinja8@users.noreply.github.com>
2026-07-25 16:24:05 -0400

    eqrqwrq

--- globals/game_state.gd
+++ globals/game_state.gd
@@ -11,7 +11,7 @@ var sfx_volume: float = 0.4
 var ambience_volume:float = 0.4
 
 var current_level
-var max_level_beaten = 15
+var max_level_beaten = 0
 
 func update_max_level_beaten():
 	max_level_beaten = max(current_level, max_level_beaten)
