093cd0ba9b5e573463c53523ab5c93e7f7e845c6
CookieFinder23 <190116541+CookieFinder23@users.noreply.github.com>
2026-07-25 23:02:39 -0400

    level draft done

--- levels/move_with_player.gd
+++ levels/move_with_player.gd
@@ -2,7 +2,7 @@ extends Area2D
 @onready var moving_stuff = $"../.."
 @onready var move_speed = moving_stuff.curve.get_baked_length() / moving_stuff.time * 2
 func _physics_process(delta: float) -> void:
-	if has_overlapping_bodies():
+	if has_overlapping_bodies() and moving_stuff.moving:
 		GameState.player.position.x += move_speed * delta
 
 
