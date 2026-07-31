c57971ac9666d1251c65fd84cbf7d2b6403ebec0
MrPuffyWuff <jasper13.com@gmail.com>
2026-07-31 12:52:10 -0400

    smooth spinning gears

--- levels/level_objects/background.gd
+++ levels/level_objects/background.gd
@@ -7,6 +7,11 @@ extends Node2D
 @onready var b3 = $Offset/Parallax2D4/BigPipes
 @onready var b4 = $Offset/Parallax2D3/SmallPipes
 
+var speed_1 : float = 0.05
+var speed_2 : float = 0.2
+var speed_3 : float = 0.15
+var speed_4 : float = 0.4
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	self.visible = true
@@ -14,7 +19,9 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	#if camera == null and GameState.player != null:
-		#camera = GameState.player.find_child("Camera2D")
-	#global_position.y = camera.global_position.y
-	pass
+	$Offset/Parallax2D2/Gear1.rotation += speed_1 * delta
+	$Offset/Parallax2D2/Gear2.rotation += speed_2 * delta
+	$Offset/Parallax2D2/Gear3.rotation -= speed_2 * delta
+	$Offset/Parallax2D2/Gear4.rotation += speed_3 * delta
+	$Offset/Parallax2D2/Gear5.rotation -= speed_3 * delta
+	$Offset/Parallax2D2/Gear6.rotation -= speed_4 * delta
